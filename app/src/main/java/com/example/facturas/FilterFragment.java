9c33175cfbcca237a0afacb719da5188e6b2a029
Marina Ruiz <marina.ruiz@viewnext.com>
2023-04-30 19:58:40 +0200

    Add filter fragment and improve layouts

---
+++ app/src/main/java/com/example/facturas/FilterFragment.java
@@ -0,0 +1,28 @@
+package com.example.facturas;
+
+import android.os.Bundle;
+
+import androidx.fragment.app.Fragment;
+
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+
+public class FilterFragment extends Fragment {
+
+    public FilterFragment() {
+        // Required empty public constructor
+    }
+
+    @Override
+    public void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+    }
+
+    @Override
+    public View onCreateView(LayoutInflater inflater, ViewGroup container,
+                             Bundle savedInstanceState) {
+        // Inflate the layout for this fragment
+        return inflater.inflate(R.layout.fragment_filter, container, false);
+    }
+}
