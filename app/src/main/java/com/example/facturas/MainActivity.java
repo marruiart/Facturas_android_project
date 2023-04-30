07e6e4fac3f953fe905f1270eef27f6bc2f9779b
MARINA RUIZ ARTACHO <6002674@viewnext.com>
2023-04-30 07:50:06 +0200

    Init project with main layout

---
+++ app/src/main/java/com/example/facturas/MainActivity.java
@@ -0,0 +1,24 @@
+package com.example.facturas;
+
+import android.os.Bundle;
+import android.util.Log;
+
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.recyclerview.widget.LinearLayoutManager;
+import androidx.recyclerview.widget.RecyclerView;
+
+import java.util.ArrayList;
+import java.util.List;
+
+import retrofit2.Call;
+import retrofit2.Callback;
+import retrofit2.Response;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
