3000dfdd9af3e759ae6779db65254aa54c855931
Marina Ruiz <marina.ruiz@viewnext.com>
2023-04-30 21:42:08 +0200

    Add open and close filter fragment from main activity

--- app/src/main/java/com/example/facturas/MainActivity.java
+++ app/src/main/java/com/example/facturas/MainActivity.java
@@ -2,19 +2,21 @@ package com.example.facturas;
 
 import android.os.Bundle;
 import android.util.Log;
+import android.view.View;
 
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.fragment.app.Fragment;
 import androidx.recyclerview.widget.LinearLayoutManager;
 import androidx.recyclerview.widget.RecyclerView;
 
 import java.util.ArrayList;
-import java.util.List;
 
 import retrofit2.Call;
 import retrofit2.Callback;
 import retrofit2.Response;
 
 public class MainActivity extends AppCompatActivity {
+    private static final String FRAGMENT_TAG = "FILTER_FRAGMENT";
     private RecyclerView mRecyclerView;
     private ArrayList<InvoiceVO> invoicesList = new ArrayList<>();
 
@@ -22,16 +24,34 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-        mRecyclerView = (RecyclerView) findViewById(R.id.recyclerView_invoices);
+        mRecyclerView = findViewById(R.id.recyclerView_invoices);
         setLayoutManager();
         enqueueInvoices();
     }
 
-    private LinearLayoutManager setLayoutManager() {
+    public void openFilterFragment(View v) {
+        FilterFragment filterFragment = new FilterFragment();
+        getSupportFragmentManager()
+                .beginTransaction()
+                .replace(R.id.fragmentContainerView, filterFragment, FRAGMENT_TAG)
+                .commit();
+    }
+
+    public void closeFilterFragment(View v) {
+        Fragment filterFragment = getSupportFragmentManager()
+                .findFragmentByTag("FILTER_FRAGMENT");
+        if (filterFragment != null) {
+            getSupportFragmentManager().
+                    beginTransaction().
+                    remove(filterFragment).
+                    commit();
+        }
+    }
+
+    private void setLayoutManager() {
         LinearLayoutManager layoutManager = new LinearLayoutManager(MainActivity.this);
         layoutManager.setOrientation(LinearLayoutManager.VERTICAL);
         mRecyclerView.setLayoutManager(layoutManager);
-        return layoutManager;
     }
 
     private void initializeAdapter() {
@@ -45,10 +65,12 @@ public class MainActivity extends AppCompatActivity {
             @Override
             public void onResponse(Call<InvoicesApiResponse> call, Response<InvoicesApiResponse> response) {
                 if (response.isSuccessful()) {
-                    List<InvoiceVO> invoices = response.body().getFacturas();
-                    Log.d("onResponse invoices", "Size of 'facturas' list: " + invoices.size());
-                    invoicesList = response.body().getFacturas();
-                    initializeAdapter();
+                    InvoicesApiResponse apiResponse = response.body();
+                    if (apiResponse != null) {
+                        invoicesList = apiResponse.getFacturas();
+                        Log.d("onResponse invoices", "Size of 'facturas' list: " + invoicesList.size());
+                        initializeAdapter();
+                    }
                 }
             }
 
