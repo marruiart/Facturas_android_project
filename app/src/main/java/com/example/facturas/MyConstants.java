cfa3716c3b48eca83fc05fc45a34c4dc3128e0c4
Marina Ruiz <marina.ruiz@viewnext.com>
2023-05-09 10:24:59 +0200

    Fix api URL

--- app/src/main/java/com/example/facturas/MyConstants.java
+++ app/src/main/java/com/example/facturas/MyConstants.java
@@ -2,7 +2,7 @@ package com.example.facturas;
 
 public final class MyConstants {
 
-    private MyConstants () {
+    private MyConstants() {
         // Private constructor to hide the implicit public one.
     }
 
@@ -12,12 +12,12 @@ public final class MyConstants {
 
     // InvoiceVO
     public static final String INVOICE_DESC_ESTADO_PAGADA = "Pagada";
-    public static final String INVOICE_DESC_ESTADO_ANULADA =  "Anulada";
+    public static final String INVOICE_DESC_ESTADO_ANULADA = "Anulada";
     public static final String INVOICE_DESC_ESTADO_CUOTA_FIJA = "Cuota fija";
     public static final String INVOICE_DESC_ESTADO_PENDIENTE = "Pendiente de pago";
     public static final String INVOICE_DESC_ESTADO_PLAN_PAGO = "Plan de pago";
 
     // Retrofit Api service
-    public static final String BASE_URL = "https://viewnextandroid.mocklab.io/";
+    public static final String BASE_URL = "https://viewnextandroid2.wiremockapi.cloud/";
     public static final String URL_PATH = "facturas";
-    }
+}
