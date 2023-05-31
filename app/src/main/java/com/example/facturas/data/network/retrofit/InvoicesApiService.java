266a3883002875a7c911903900ddc5b7ae0e04ff
Marina Ruiz <marina.ruiz@viewnext.com>
2023-05-31 10:30:37 +0200

    Add MockCircular to retromock service

--- app/src/main/java/com/example/facturas/data/network/retrofit/InvoicesApiService.java
+++ app/src/main/java/com/example/facturas/data/network/retrofit/InvoicesApiService.java
@@ -4,13 +4,17 @@ import com.example.facturas.data.model.InvoicesApiResponse;
 import com.example.facturas.utils.MyConstants;
 
 import co.infinum.retromock.meta.Mock;
+import co.infinum.retromock.meta.MockCircular;
 import co.infinum.retromock.meta.MockResponse;
 import retrofit2.Call;
 import retrofit2.http.GET;
 
 public interface InvoicesApiService {
     @Mock
-    @MockResponse(body = "invoices")
+    @MockCircular
+    @MockResponse(body = "invoices.json")
+    @MockResponse(body = "paid_invoices.json")
+    @MockResponse(body = "pending_invoices.json")
     @GET(MyConstants.URL_PATH)
     Call<InvoicesApiResponse> getInvoices();
 }
--- app/src/main/java/com/example/facturas/data/network/retrofit/ResourceBodyFactory.java
+++ app/src/main/java/com/example/facturas/data/network/retrofit/ResourceBodyFactory.java
@@ -15,7 +15,18 @@ public final class ResourceBodyFactory implements BodyFactory {
     @Override
     public InputStream create(@NonNull final String input) throws IOException {
         // this will throw if input is empty string, regular class loader opens a stream to directory
-        return App.getContext().getResources().openRawResource(R.raw.invoices);
+        InputStream inputStream;
+        switch (input) {
+            case "paid_invoices.json":
+                inputStream = App.getContext().getResources().openRawResource(R.raw.paid_invoices);
+                break;
+            case "pending_invoices.json":
+                inputStream = App.getContext().getResources().openRawResource(R.raw.pending_invoices);
+                break;
+            default:
+                inputStream = App.getContext().getResources().openRawResource(R.raw.invoices);
+        }
+        return inputStream;
     }
 }
 
